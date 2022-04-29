44ec39f8b789926f952e33609194e04e3a1ac5a1
Martino888 <maxmartino33@gmail.com>
2022-04-29 17:00:44 +0200

    inserimento set--

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- Oggi pomeriggio ripassate i primi concetti di classe e di variabili e
+<!-- I primi concetti di classe e di variabili e
 metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
 - è definita una classe ‘Movie’
    => all'interno della classe sono dichiarate delle variabili d'istanza
@@ -13,6 +13,35 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
         protected $duration;
         protected $genre;
         protected $date;
+
+        function __construct($_title, $lenguage, $duration, $genre, $date)
+        {
+            $this->title = $_title;
+            $this->lenguage = $_lenguage;
+            $this->duration = $_duration;
+            $this->genre = $_genre;
+            $this->date = $_date;
+        }
+
+        function setTitle($title){
+            $this->title = $title;
+        }
+
+        function setLenguage($lenguage){
+            $this->lenguage = $lenguage;
+        }
+
+        function setDuration($duration){
+            $this->duration = $duration . "minutes";
+        }
+
+        function setgenre($genre){
+            $this->genre = $genre;
+        }
+
+        function setDate($date){
+            $this->date = $date . "date";
+        }
     }
 
 
@@ -26,14 +55,14 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"
+    <link rel="stylesheet" href="style.css">
     <title>Movie</title>
 </head>
 <body>
-    <?php
 
+<div class="container">
+<h1><? echo $title; ?></h1>
+</div>
 
-
-    ?>
 </body>
 </html>
